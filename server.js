5b36a01aa4dd1cbdd9fca4e34a0de7222bc57124
Mhassan799 <93198093+Mhassan799@users.noreply.github.com>
2023-05-28 09:52:31 +0500

    assignment 3 completed

---
+++ server.js
@@ -0,0 +1,19 @@
+// console.log("hello world");
+const express = require('express');
+const app = express();
+require('dotenv').config({path: './config.env'})
+const connectDB = require('./db/db')
+const routerUser = require('./route/userRoute')
+const port = 5000  || process.env.PORT
+//connect databse
+connectDB()
+
+//mmiddleware
+app.use(express.json())
+app.use('/api', routerUser)
+//routes
+
+//coonect app
+app.listen(port, ()=>{
+    console.log(`server is runing on port ${port}`)
+})
